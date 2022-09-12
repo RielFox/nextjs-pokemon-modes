75ea893f8b263c5bf8e75ae36e6c8e6a85a96868
unknown <riel.fox4@gmail.com>
2022-09-12 04:08:42 +0300

    Moving to SSR - Server-Side Rendering

---
+++ pokemon-demo-Server-Side-Rendering/pages/index.js
@@ -0,0 +1,83 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+// No need for useState or useEffect anymore
+import React from "react"
+
+// import next.js router
+import Link from "next/link"
+
+// Causes dependency bugs
+//import { response } from 'express'
+
+// We need to do this request (fetch) at server side time, 
+// thus we use a new exported asynchronous function called 'getServerSideProps'
+export async function getServerSideProps() {
+  // What it does is that it makes any requests that you need to any services,
+  // gethers all that data and returns an object that has props in it
+  // and then those props are sent to the react component which renders them at server-time
+  
+  // Get http response
+  const res = await fetch('https://t6nnv61cec.execute-api.eu-central-1.amazonaws.com/dev/static', {
+    headers: {
+      'Content-Type': 'application/json',
+    } 
+  })
+
+  // Return an object with props in it
+  // The props will have 'pokemon' as the name of of the prop
+  // and the value will be the json list of pokemon
+  return {
+    props: {
+      pokemon: await res.json(),
+    }
+  }
+  
+}
+
+// Here, we take pokemon as a prop passed from getServerSideProps()
+// We get rid of all the Client-Side code (useState-useEffect) (commented)
+export default function Home( {pokemon} ) {
+
+  // Client-Side code is not needed anymore
+  // // Array of pokemon, index page is the list page
+  // const [pokemon, setPokemon] = useState([])
+
+  // // We use useEffect but only on start-up
+  // useEffect(() => {
+  //   ...
+  // }, [])
+
+  // If you view page source, all of the fetched pokemon will appear
+
+  return (
+    <div className={styles.container}>
+      {/* <div>{JSON.stringify(pokemon)}</div> */}
+      <Head>
+        <title>Pokemon List</title>
+
+      </Head>
+
+      <div className={styles.grid}>
+
+        {pokemon.map((pokemon) => 
+
+          <div className={styles.card} key={pokemon.id}>
+            
+            <Link href={`/pokemon/${pokemon.id}`}>
+              <a>
+                <img src={`https://pokemon-next-tutorial.s3.eu-central-1.amazonaws.com/${pokemon.image}`} alt={pokemon.name} />
+                <h3>{pokemon.name}</h3>
+              </a>
+            </Link>
+            
+          </div>
+
+        )}
+
+      </div>
+      
+    </div>
+  )
+
+}
---
+++ pokemon-demo-Server-Side-Rendering/pages/pokemon/[id].js
@@ -0,0 +1,131 @@
+// Need router to find out which id we are requesting
+// import { useRouter } from "next/router"
+
+// import React, { useState, useEffect } from "react"
+
+import React from "react"
+import Head from 'next/head'
+import styles from '../../styles/Details.module.css'
+import Link from "next/link"
+
+// getServerSideProps takes a context object
+// and in that context are the params object
+// that params object includes the 'id'
+export async function getServerSideProps( { params } ) {
+    // Get http response
+    const res = await fetch(`https://t6nnv61cec.execute-api.eu-central-1.amazonaws.com/dev/${params.id}.json`, {
+        headers: {
+          'Content-Type': 'application/json',
+        } 
+    })
+
+    return {
+        props: {
+            pokemon: await res.json()
+        }
+    }
+}
+
+// Each specific Pokemon id is mapped here
+// Returns a page with details of the specific pokemon having that id
+export default function Details( { pokemon } ) {
+// --- Comment out client-side rendering code ---
+//     // Get id from query
+//     const {
+//         query: {id},
+//     } = useRouter();
+
+//     // Bring in useEffect and useState 
+//     // Pokemon, in this case, isn't going to be an array but a single pokemon object (json)
+//     // Detail object (json) returned has the name, type and stats for each Pokemon
+//     // We don't want an array so not useState([]), use useState(null) - 
+//     // null will be replaced by an object, once we get it
+//     const [pokemon, setPokemon] = useState(null)
+
+//   // We use useEffect but only on start-up
+//   useEffect(() => {
+//     async function getPokemon() {
+
+//         const res = await fetch(`https://t6nnv61cec.execute-api.eu-central-1.amazonaws.com/dev/${params.id}.json`, {
+//             headers: {
+//               'Content-Type': 'application/json',
+//             } 
+//         })
+      
+//       // Set pokemon to be the .json version fo the json string received
+//       const res_json = await res.json()
+
+//       setPokemon(res_json)
+//       //console.log(res_json)
+//     }
+
+//     // If there is an id, get the data
+//     if (id){
+//         getPokemon()
+//     }
+//   }, 
+//   // We need to run this effect whenever id changes
+//   [id])
+  
+//   // If object is empty (no pokemon)
+//   if (!pokemon) {
+//     return null;
+//   }
+
+    return <div>
+        
+        {/* {JSON.stringify(pokemon)} */}
+        {/* Head with Pokemon's name as page title */}
+        <Head>
+            <title>{pokemon.name}</title>
+        </Head>
+
+        {/* A link back to the index directory, listing the Pokemon */}
+        <div>
+            <Link href="/">
+                <a>Back to Home</a>
+            </Link>
+        </div>
+
+        {/* Content  */}
+
+        <div className={styles.layout}>
+            <div>
+                {/* First Column - Image */}
+                <img 
+                className={styles.picture}
+                src={`https://pokemon-next-tutorial.s3.eu-central-1.amazonaws.com/${pokemon.image}`}
+                alt={pokemon.name.english}
+                />
+            </div>
+
+            {/* Second Column - Info */}
+            <div>
+                {/* Pokemon Name */}
+                <div className={styles.name}>{pokemon.name}</div>
+                {/* Pokemon Type - Type is an array so we join using comma ',' - e.g. "Grass, Poison" */}
+                <div className={styles.type}>{pokemon.type.join(',')}</div>
+                {/* Pokemon Table of Attributes */}
+                <table>
+                    <thead className={styles.header}>
+                        <tr>
+                            <th style={{textAlign: "left"}}>Name</th>
+                            <th style={{textAlign: "right"}}>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* Iterate through list of stats - Get name and value and create a row in table for each one */}
+                        {pokemon.stats.map(({ name, value }) => (
+                            <tr key={name}>
+                                <td className={styles.attribute}>{name}</td>
+                                <td style={{textAlign: "right"}}>{value}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+        
+    </div>
+}
